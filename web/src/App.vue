30ccc61677be97747fe6ed62ac89abecd0406d5b
erinc <yigit.erinc@ozu.edu.tr>
2018-11-01 15:01:48 +0300

    implement the 404 Not Found Page

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,25 +4,6 @@
   </div>
 </template>
 
-<script>
-  import homePage from './components/HomePage/HomePage'
-  import loginPage from './components/LoginPage/LoginPage'
-  import gamesPage from './components/GamesPage/GamesPage'
-  import adminPage from './components/AdminPage/AdminPage'
-  import calendar from './components/AdminPage/Calendar'
-
-  export default {
-  name: 'App',
-    components: {
-      homePage,
-      loginPage,
-      gamesPage,
-      adminPage,
-      calendar
-    }
-  }
-</script>
-
 <style scoped>
   .overlay {
     text-align: center;
--- web/src/main.js
+++ web/src/main.js
@@ -10,28 +10,28 @@ import HomePage from "./components/HomePage/HomePage"
 import AdminPage from "./components/AdminPage/AdminPage"
 import GamesPage from "./components/GamesPage/GamesPage"
 import LoginPage from "./components/LoginPage/LoginPage"
-
-Vue.use(BootstrapVue);
-Vue.use(VueRouter);
-Vue.config.productionTip = false;
+import NotFound from "./components/NotFoundPage/NotFound"
 
 const router = new VueRouter({
   routes: [
     {path: '/', component: HomePage},
     {path: '/admin', component: AdminPage},
     {path: '/games', component: GamesPage},
-    {path: '/adminlogin', component: LoginPage}
+    {path: '/adminlogin', component: LoginPage},
+    {path: '/404', component: NotFound},
+    {path: '*', redirect: '/404' }
   ],
   mode: 'history'
 });
+
+Vue.use(BootstrapVue);
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: {App},
-  template: '<App/>',
+  template: '<App/>'
 })
-
-
-
