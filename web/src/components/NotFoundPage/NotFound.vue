41b40ca9a7ce658645fadfb84aa71b1bec135e29
erinc <yigit.erinc@ozu.edu.tr>
2018-11-01 15:04:57 +0300

    fix the directory issue in the last commit

---
+++ web/src/components/NotFoundPage/NotFound.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <b-container>
+      <b-row class="row">
+        <b-col class="col" sm="12">
+          <h1>404 Not Found!</h1>
+          <p>But you've found us!</p>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col class="col" sm="2"></b-col>
+        <b-col class="col" sm="6"><img src="../../assets/surprise.jpg" alt=""></b-col>
+        <b-col class="col" sm="4"></b-col>
+      </b-row>
+
+      <b-row>
+      <b-col class="col" sm="4"></b-col>
+      <b-col class="col" sm="4" align="middle">
+        <br>
+        <b>Are you lost?</b>
+        <br>
+        <i class="tolkien">
+          “All that is gold does not glitter,
+          Not all those who wander are lost;
+          The old that is strong does not wither,
+          Deep roots are not reached by the frost.
+
+          From the ashes a fire shall be woken,
+          A light from the shadows shall spring;
+          Renewed shall be blade that was broken,
+          The crownless again shall be king.”
+          <br>
+          -J.R. Tolkien
+          <br>
+        </i>
+        <br>
+        <router-link to="/">Go to Homepage</router-link>
+      </b-col>
+      <b-col class="col" sm="4"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
