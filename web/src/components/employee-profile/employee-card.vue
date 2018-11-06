3b905416f04e2663680796677b18931328349715
erinc <yigit.erinc@ozu.edu.tr>
2018-11-07 01:27:42 +0300

    add work completed column to EMPLOYEE table, and show that information on frontend, display the manager name for each employee and make the work completed info editable for the managers of that employee

--- web/src/components/employee-profile/employee-card.vue
+++ web/src/components/employee-profile/employee-card.vue
@@ -9,9 +9,13 @@
           <hr class="card-separator">
           <p class="card-text text-left"><b>Title<br></b> {{ employeeCardDto.titleName }} </p>
           <p class="card-text text-left"><b>Department<br></b> {{ employeeCardDto.departmentName }} </p>
+          <p class="card-text text-left"><b> Manager </b><br> {{ employeeCardDto.employeeManagerFullName }} </p>
           <p class="card-text text-left"><b>Email<br></b> {{ employeeCardDto.employeeEmail }} </p>
           <p class="card-text text-left"><b>Phone Number<br></b> {{ employeeCardDto.employeePhoneNumber }} </p>
-          <p class="card-text text-left"><b>Time Since Join (Days)<br></b> {{ employeeCardDto.timeSinceJoin }} </p>
+          <p class="card-text text-left"><b> Completed Work (Hours) </b><br>{{ employeeCardDto.employeeCompletedWorkInHours }}
+            <WorkInfoManagerEdit v-if=isManagerIdEqualToLoginID()></WorkInfoManagerEdit>
+          </p>
+          <p class="card-text text-left"><b> Time Since Join (Days) </b><br> {{ employeeCardDto.timeSinceJoin}}</p>
         </div>
       </b-card>
 
@@ -21,14 +25,28 @@
 </template>
 
 <script>
-  import CommonMixin from '../../mixins/common-mixin'
+  import WorkInfoManagerEdit from './work-info-manager-edit.vue'
 
   export default {
-    mixins: [CommonMixin],
+    components: {
+      WorkInfoManagerEdit
+    },
     props: ['employeeCardDto', 'employeeImage'],
     data () {
       return {
-        name: 'employeeCard'
+        name: 'employeeCard',
+        placeholderId: 0
+      }
+    },
+    methods: {
+      isManagerIdEqualToLoginID: function() {
+        if (this.employeeCardDto.employeeManagerId !== null) {
+          let managerId = this.employeeCardDto.employeeManagerId
+
+          return managerId === this.placeholderId;
+        } else {  // if the employee has no manager
+          return true;
+        }
       }
     }
   }
---
+++ web/src/components/employee-profile/work-info-manager-edit.vue
@@ -0,0 +1,41 @@
+<template>
+  <span>
+    <b-btn class="triggerButton" v-b-modal.edit-completed-hours variant="outline-success" size="sm">+</b-btn>
+    <!-- Modal Component -->
+      <b-modal id="edit-completed-hours"
+               title="Edit Completed Hours"
+               ok-title="Save"
+               ok-variant="outline-success"
+               cancel-title="Cancel"
+               cancel-variant="outline-danger"
+               size="md" centered>
+        <div class="modal-body">
+          <b-form-input v-model="modificationAmount" type="number" placeholder="Work Hours"></b-form-input> <br>
+          <div>
+            <b-row class="row">
+              <b-col class="col-sm-2"></b-col>
+              <b-col class="col-sm-4"><b-btn variant="secondary">Substract Hours</b-btn></b-col>
+              <b-col class="col-sm-4"><b-btn variant="primary">Add Hours</b-btn></b-col>
+              <b-col class="col-sm-2"></b-col>
+            </b-row>
+          </div>
+        </div>
+      </b-modal>
+  </span>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        modificationAmount
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .triggerButton {
+    margin-left: 10px;
+  }
+</style>
