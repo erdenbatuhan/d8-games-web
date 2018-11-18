3e70fdb233f0dbd41cdce4d98916e943e4c7a6d1
CEMAL <baris.onur@ozu.edu.tr>
2018-11-18 18:07:48 +0300

    implement the qr reader for android devices

--- authenticator/android/app/src/main/java/com/example/bar/qrscanner/MainActivity.java
+++ authenticator/android/app/src/main/java/com/example/bar/qrscanner/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.bar.qrscanner;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.SparseArray;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -20,21 +27,24 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    SurfaceView cameraPreview;
-    BarcodeDetector barcodeDetector;
-    CameraSource cameraSource;
-    final int RequestCameraPermissionID = 1001;
+    private static final int REQUEST_CAMERA_PERMISSION_ID = 1001;
+    private static final int VIBRATION_DURATION = 250;
+    private static final int SLEEP_AFTER_READ = 5000;
 
+    private SurfaceView cameraPreview;
+    private TextView barcodeInfo;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private boolean qrCodeReadable;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
-            case RequestCameraPermissionID: {
+            case REQUEST_CAMERA_PERMISSION_ID: {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
                         return;
-                    }
+
                     try {
                         cameraSource.start(cameraPreview.getHolder());
                     } catch (IOException e) {
@@ -47,10 +57,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        qrCodeReadable = true;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         cameraPreview = (SurfaceView) findViewById(R.id.cameraPreview);
+
+        barcodeInfo = (TextView) findViewById(R.id.status_text);
+        barcodeInfo.setVisibility(View.INVISIBLE);
+
         barcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.QR_CODE)
                 .build();
@@ -60,12 +76,12 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String []{Manifest.permission.CAMERA},RequestCameraPermissionID);
-                }
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                    ActivityCompat.requestPermissions(MainActivity.this, new String []{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION_ID);
+
                 try {
                     cameraSource.start(cameraPreview.getHolder());
                 } catch (IOException e){
@@ -75,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
             }
 
             @Override
@@ -84,13 +101,46 @@ public class MainActivity extends AppCompatActivity {
         });
 
         barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+
             @Override
             public void release() {
+
             }
 
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
-                SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+                final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+
+                if (qrcodes.size() != 0 && qrCodeReadable) {
+                    qrCodeReadable = false;
+
+                    barcodeInfo.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO: SERVICES IMPLEMENTATION HERE: qrcodes.valueAt(0).displayValue
+
+                            Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(VIBRATION_DURATION);
+
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                            builder.setTitle("Success");
+                            builder.setMessage("QR Code is successfully read!")
+                                    .setCancelable(false)
+                                    .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            qrCodeReadable = true;
+                                            System.exit(0);
+                                        }
+                                    });
+
+                            builder.create().show();
+                        }
+                    });
+                }
             }
         });
     }
